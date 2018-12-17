668954fe91c49574707ba8876494d083695fdd6e
Dave <dhurls99@gmail.com>
2018-12-17 08:46:18 -0500

    add alt tages

--- src/containers/Hayley.js
+++ src/containers/Hayley.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default () =>
   <div className="Hayley">
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/1.png" /></div>
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/2.png" /></div>
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/3.png" /></div>
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/4.png" /></div>
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/5.png" /></div>
-    <div><img src="https://s3.amazonaws.com/notes-app-client99/6.png" /></div>
+    <div><img alt="image1" src="https://s3.amazonaws.com/notes-app-client99/1.png" /></div>
+    <div><img alt="image2" src="https://s3.amazonaws.com/notes-app-client99/2.png" /></div>
+    <div><img alt="image3" src="https://s3.amazonaws.com/notes-app-client99/3.png" /></div>
+    <div><img alt="image4" src="https://s3.amazonaws.com/notes-app-client99/4.png" /></div>
+    <div><img alt="image5" src="https://s3.amazonaws.com/notes-app-client99/5.png" /></div>
+    <div><img alt="image6" src="https://s3.amazonaws.com/notes-app-client99/6.png" /></div>
   </div>
