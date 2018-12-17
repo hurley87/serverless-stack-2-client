86d26228383f457ae6fb6602505864faeb863d05
Dave <dhurls99@gmail.com>
2018-12-17 08:57:57 -0500

    test

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -45,14 +45,24 @@ export default class Settings extends Component {
 	render() {
 	  return (
 	    <div className="Settings">
-	      <StripeProvider apiKey={config.STRIPE_KEY}>
-	        <Elements>
-	          <BillingForm
-	            loading={this.state.isLoading}
-	            onSubmit={this.handleFormSubmit}
-	          />
-	        </Elements>
-	      </StripeProvider>
+
+{
+	      // <StripeProvider apiKey={config.STRIPE_KEY}>
+	      //   <Elements>
+	      //     <BillingForm
+	      //       loading={this.state.isLoading}
+	      //       onSubmit={this.handleFormSubmit}
+	      //     />
+	      //   </Elements>
+	      // </StripeProvider>
+
+}
+		    <div><img alt="image1" src="https://s3.amazonaws.com/notes-app-client99/1.png" /></div>
+		    <div><img alt="image2" src="https://s3.amazonaws.com/notes-app-client99/2.png" /></div>
+		    <div><img alt="image3" src="https://s3.amazonaws.com/notes-app-client99/3.png" /></div>
+		    <div><img alt="image4" src="https://s3.amazonaws.com/notes-app-client99/4.png" /></div>
+		    <div><img alt="image5" src="https://s3.amazonaws.com/notes-app-client99/5.png" /></div>
+		    <div><img alt="image6" src="https://s3.amazonaws.com/notes-app-client99/6.png" /></div>
 	    </div>
 	  );
 	}
