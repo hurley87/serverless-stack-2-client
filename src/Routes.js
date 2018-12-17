417232fd3844ad6e9ce8c9ca9fb7ab9cd024a083
Dave <dhurls99@gmail.com>
2018-12-17 01:13:56 -0500

    hayley

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
+import Hayley from "./containers/Hayley";
 import AppliedRoute from "./components/AppliedRoute";
 import Settings from "./containers/Settings";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
@@ -19,6 +20,7 @@ export default ({ childProps }) =>
     <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
     <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+    <AuthenticatedRoute path="/hayley" exact component={Hayley} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
---
+++ src/containers/Hayley.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () =>
+  <div className="Hayley">
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/1.png" /></div>
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/2.png" /></div>
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/3.png" /></div>
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/4.png" /></div>
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/5.png" /></div>
+    <div><img src="https://s3.amazonaws.com/notes-app-client99/6.png" /></div>
+  </div>
